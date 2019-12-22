686589c7cb63df7ee8b8d6d463ee2b6e161012e0
Mikko Suni <msoft@hotmail.fi>
2019-12-22 18:54:24 +0200

    Added price and product amout handling to shop page

--- src/pages/shop/shop.ts
+++ src/pages/shop/shop.ts
@@ -30,6 +30,8 @@ export class ShopPage {
   searchResult: any;
   
   productInfo = { objectId:'', ISBN:'', productName:'', price:'', amountInStock:'', productCode:'', availableFromPublisher:'' };
+  productsInCart = 0;
+  totalSum = 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public productList: ProductList, private shoppingCart: ShoppingcartProvider) {
   }
@@ -38,6 +40,7 @@ export class ShopPage {
     console.log('ionViewDidLoad ShopPage');
     this.productList.getProductInfo();
     this.cartContent = this.shoppingCart.getProducts();
+    this.update();
   }
   
   clear() {
@@ -49,7 +52,7 @@ export class ShopPage {
     }
   
   onProductNumberUpdated() {
-      console.log('>> home.onProductNumberUpdated: ' + this.productNumberInitials);
+      console.log('>> shop.onProductNumberUpdated: ' + this.productNumberInitials);
       if (this.productNumberInitials.length > 0) {
           var found = this.productList.getProductProgressivelyByNumber(this.productNumberInitials);
           this.searchResult = found;
@@ -64,31 +67,32 @@ export class ShopPage {
   }
 
   onProductNameUpdated() {
-      console.log('>> home.onProductNameUpdated: ' + this.productNameInitials);
+      console.log('>> shop.onProductNameUpdated: ' + this.productNameInitials);
       var found = this.productList.getProductByName(this.productNameInitials);
       this.searchResult = found;
       console.log('>> found: ' + found.length);
       console.log('>> json: ' + JSON.stringify(found));
       this.clear();
-      if (found.length == 1) {
-          console.log('>> found item: ' + found[0].productName);
-          this.showProduct(found[0]);
-      }
+      //if (found.length == 1) {
+      //    console.log('>> found item: ' + found[0].productName);
+      //    this.showProduct(found[0]);
+      //}
   }
   
   onProductSelected(productName, index) {
-      console.log('>> home.onProductSelected: ' + productName + ' index: ' + index);
+      console.log('>> shop.onProductSelected: ' + productName + ' index: ' + index);
       this.showProduct(this.searchResult[index]);
       this.searchResult.splice(0,this.searchResult.length)
     }
 
   addToShoppingCart(productInfo) {
       this.shoppingCart.addProduct(productInfo);
+      this.update();
       //this.cartContent = this.shoppingCart.getProducts();
   }
   
   showProduct(productInfo) {
-      console.log('>> home.showProduct: ' + JSON.stringify(productInfo));
+      console.log('>> shop.showProduct: ' + JSON.stringify(productInfo));
       this.addToShoppingCart(productInfo);
       this.productInfo = productInfo;
       this.productNumber = productInfo.ISBN;
@@ -100,5 +104,28 @@ export class ShopPage {
       } else {
           this.inProductionInfo = "";
       }
+  }
+
+  removeProduct(productName, i) {
+    console.log('removeFromCart: ' + productName + ", index: " + i);
+    this.shoppingCart.removeProduct(i);
+    this.update();
+  }
+  
+  increase(productName, i) {
+      console.log('increase: ' + productName + ", index: " + i);
+      this.shoppingCart.increase(i);
+      this.update();
+  }
+  
+  decrease(productName, i) {
+      console.log('decrease: ' + productName + ", index: " + i);
+      this.shoppingCart.decrease(i);
+      this.update();
+  }
+
+  update() {
+    this.productsInCart = this.shoppingCart.productsInCart;
+    this.totalSum = this.shoppingCart.totalSum;
   }
 }
